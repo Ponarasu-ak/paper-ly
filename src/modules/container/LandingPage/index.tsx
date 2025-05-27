4003ba50e63d92b72a3101ba7434b8eded732d13
GOKUL\gokul <ponarasu.ak@gmail.com>
2025-05-27 07:47:22 +0530

    no message

--- src/modules/container/LandingPage/index.tsx
+++ src/modules/container/LandingPage/index.tsx
@@ -5,22 +5,23 @@ import { ButtonMui, TextUndeline } from "../../../components";
 const LandingPage = () => {
   const navigate = useNavigate();
 
-  const handleNavigation = () => {
-    navigate("/signin");
-  };
+  // const handleNavigation = () => {
+  //   navigate("/signin");
+  // };
 
   return (
     <div
       style={{
         height: "100vh",
         backgroundColor: "#fff",
+
       }}
       className="flex flex-col"
     >
       {/* <Navbar enableOverview/> */}
       <div className="flex justify-between items-center px-5 md:px-10 py-6">
         <div className="cursor-pointer" onClick={() => navigate("/")}>
-          <img src={Logo} width={140} />
+          <img src={Logo} width={110} />
         </div>
         <div>
           <TextUndeline text="Overview" />
@@ -33,41 +34,29 @@ const LandingPage = () => {
             textAlign: "center",
             lineHeight: "1.4",
           }}
-          className="text-6xl font-extrabold  md:text-7xl md:font-bold"
+          className=" text-6xl    font-extrabold  md:text-7xl md:font-bold"
         >
           Write Your Thoughts,
           <br /> Stay Clear.
         </div>
         <div
           style={{ color: "var(--description)", textAlign: "center" }}
-          className="text-xl md:text-2xl font-medium"
+          className="text-lg md:text-2xl font-medium"
         >
           Simple. Your notes, done right.
         </div>
         <div className="mt-6">
           <ButtonMui
-            btnLable="Try NoteBook"
+            btnLable="Try Paperly"
             btnColor="var(--primary)"
             btnPadding="10px 20px"
             btnVarient="outlined"
             btnRadius="50px"
             btnEndIcon={<ArrowRight />}
-            btnOnclick={handleNavigation}
+            // btnOnclick={handleNavigation}
           />
         </div>
       </div>
-
-      <style>
-        {`
-      @keyframes gradientWave {
-      0% { background-position: 0% 50%; }
-      25% { background-position: 50% 75%; }
-      50% { background-position: 100% 50%; }
-      75% { background-position: 50% 25%; }
-      100% { background-position: 0% 50%; }
-      }
-      `}
-      </style>
     </div>
   );
 };
